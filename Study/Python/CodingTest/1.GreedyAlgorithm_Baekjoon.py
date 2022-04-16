2db5550ce7be8e7c2fd6c77ca8dcbf8fdd88c808
Youngho Lee <emfo0124@naver.com>
2022-04-16 16:00:03 +0900

    Update 1.GreedyAlgorithm_Baekjoon.py

--- Study/Python/CodingTest/1.GreedyAlgorithm_Baekjoon.py
+++ Study/Python/CodingTest/1.GreedyAlgorithm_Baekjoon.py
@@ -58,3 +58,41 @@ for i in range(len(data)):
     time_total += time_per
 
 print(time_total)
+
+
+## 4) 동전 0
+# 준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다. 동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다. 
+# 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
+
+n, k = map(int, input().split())
+coin_list = []
+for i in range(n):
+    coin_list.append(int(input()))
+    
+count = 0
+for i in reversed(range(n)):
+    count += k // coin_list[i]
+    k %= coin_list[i]
+    
+print(count)
+
+
+## 5) 보물
+# 옛날 옛적에 수학이 항상 큰 골칫거리였던 나라가 있었다. 이 나라의 국왕 김지민은 다음과 같은 문제를 내고 큰 상금을 걸었다.
+# 길이가 N인 정수 배열 A와 B가 있다. 다음과 같이 함수 S를 정의하자.
+# S = A[0] × B[0] + ... + A[N-1] × B[N-1]
+# S의 값을 가장 작게 만들기 위해 A의 수를 재배열하자. 단, B에 있는 수는 재배열하면 안 된다.
+# S의 최솟값을 출력하는 프로그램을 작성하시오.
+
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+
+result = 0
+for i in range(n):
+    result += (A[i] * B[i])
+    
+print(result)
