90d57352698826fded52ba9f28005ef7364ffe77
Youngho Lee <emfo0124@naver.com>
2022-10-05 17:13:47 +0900

    Update app.js

--- Study/Front-end/JavaScript/[NC] Chrome App/app.js
+++ Study/Front-end/JavaScript/[NC] Chrome App/app.js
@@ -1,38 +1,7 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    h1.style.color = "blue";
-}
-
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("All Good!");
+    h1.classList.toggle("clicked")
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
