4bff663e9a74576538db1d03bf274b15ed4f067e
Youngho Lee <emfo0124@naver.com>
2022-10-28 17:37:07 +0900

    Create toDos.js

---
+++ Study/Front-end/React/[NC] Movie App/Practice/toDos.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [toDo, setToDo ] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos(currentArray => [toDo, ...currentArray]);
+  }
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          value={toDo} 
+          type="text" 
+          placeholder="Write your to do..." 
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
