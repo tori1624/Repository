25ebb1a83f1d72ade51f91aa91a421e1b743b05c
Youngho Lee <emfo0124@naver.com>
2022-10-31 17:53:27 +0900

    Create coinTracker.js

---
+++ Study/Front-end/React/[NC] Movie App/Practice/coinTracker.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [balance, setBalance] = useState(0);
+  useEffect (() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+}
+
+export default App;
