af6520e8fdfa4bcc15ee64b7ca87c24cd63b76b6
Youngho Lee <emfo0124@naver.com>
2022-11-29 13:35:50 +0900

    Update Detail.js

--- Study/Front-end/React/[NC] Movie App/routes/Detail.js
+++ Study/Front-end/React/[NC] Movie App/routes/Detail.js
@@ -1,5 +1,19 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>
+  const {id} = useParams();
+  const [movie, setMovie] = useState([]);
+  const getMovie = async() => {
+    const json = await (
+      await fetch( `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie.title)
+  }
+  useEffect(() => {
+    getMovie();
+  }, [])
+  return <h1>{movie}</h1>
 }
 
 export default Detail;
