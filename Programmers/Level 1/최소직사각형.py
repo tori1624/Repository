1048e2effeebf7429f7be8abfa026ccdf1f297e8
Youngho Lee <emfo0124@naver.com>
2024-04-15 16:20:45 +0900

    Update 최소직사각형.py

--- Programmers/Level 1/최소직사각형.py
+++ Programmers/Level 1/최소직사각형.py
@@ -4,14 +4,15 @@ def solution(sizes):
     max_h = 0
     
     for size in sizes:
-        
+        # 명함을 가로로 눕히는 경우 고려
         if size[0] < size[1]:
             tmp_w = size[1]
             tmp_h = size[0]
         else:
             tmp_w = size[0]
             tmp_h = size[1]
-        
+
+        # 폭과 높이의 최댓값 업데이트
         if tmp_w > max_w:
             max_w = tmp_w
         
