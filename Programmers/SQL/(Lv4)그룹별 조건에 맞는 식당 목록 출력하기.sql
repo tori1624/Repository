d406daa9f7d21e43473f01616d6d8182e8b20c24
Youngho Lee <emfo0124@naver.com>
2024-04-19 13:25:36 +0900

    Update (Lv4)그룹별 조건에 맞는 식당 목록 출력하기.sql

--- Programmers/SQL/(Lv4)그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ Programmers/SQL/(Lv4)그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -3,9 +3,11 @@ SELECT B.MEMBER_NAME
      , TO_CHAR(A.REVIEW_DATE, 'YYYY-MM-DD') AS REVIEW_DATE
 FROM REST_REVIEW A
 JOIN MEMBER_PROFILE B ON A.MEMBER_ID = B.MEMBER_ID
+--리뷰 수를 바탕으로 리뷰를 가장 많이 작성한 회원 ID 추출
 WHERE A.MEMBER_ID IN (SELECT MEMBER_ID
                       FROM REST_REVIEW
                       GROUP BY MEMBER_ID
+                      --가장 많이 작성한 리뷰의 수
                       HAVING COUNT(MEMBER_ID) = (SELECT MAX(COUNT(MEMBER_ID))
                                                  FROM REST_REVIEW
                                                  GROUP BY MEMBER_ID
