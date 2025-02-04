85cbeb4dbefb6a9407541932919a89068932f3af
Youngho Lee <emfo0124@naver.com>
2025-02-04 11:37:25 +0900

    Update meme_launchpad.sql

--- Project/Dune/PumpSpace/meme_launchpad.sql
+++ Project/Dune/PumpSpace/meme_launchpad.sql
@@ -1,18 +1,37 @@
-with launched_cnt as (
-select date_trunc('day', block_time) as period
-     , count(*) as launched
-from tokens_avalanche_c.transfers
-where "from" = 0x0000000000000000000000000000000000000000
-and "to" = 0x096f6df3d0dB9617771C4689338a8d663810140c
-group by 1
+with launched_tokens as (
+    -- using $AVAX
+    select block_time
+    from avalanche_c.transactions
+    where "to" = 0x096f6df3d0dB9617771C4689338a8d663810140c
+    and substring(data from 1 for 4) = 0x06d91d41
+    and success
+    
+    union all
+
+    -- using $PEARL
+    select block_time
+    from avalanche_c.transactions
+    where "to" = 0x096f6df3d0dB9617771C4689338a8d663810140c
+    and substring(data from 1 for 4) = 0x7fddb4fa
+    and success
+),
+launched_cnt as (
+    select date_trunc('day', block_time) as period
+         , count(*) as launched
+    from launched_tokens
+    group by 1
+),
+listed_tokens as (
+    select block_time
+    from tokens_avalanche_c.transfers
+    where "from" = 0x096f6df3d0dB9617771C4689338a8d663810140c
+    and "to" = 0x000000000000000000000000000000000000dEaD
 ),
 listed_cnt as (
-select date_trunc('day', block_time) as period
-     , count(*) as listed
-from tokens_avalanche_c.transfers
-where "from" = 0x096f6df3d0dB9617771C4689338a8d663810140c
-and "to" = 0x000000000000000000000000000000000000dEaD
-group by 1
+    select date_trunc('day', block_time) as period
+         , count(*) as listed
+    from listed_tokens
+    group by 1
 )
 
 select period
