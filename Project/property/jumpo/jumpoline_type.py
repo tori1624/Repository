8907ba21ad71d2dd155ec0d29acc33d2a1c5b4c9
Youngho Lee <emfo0124@naver.com>
2025-03-13 16:43:31 +0900

    업종별 추가 페이지 크롤링 코드 추가

--- Project/property/jumpo/jumpoline_type.py
+++ Project/property/jumpo/jumpoline_type.py
@@ -18,7 +18,7 @@ columns = ['ad_type', 'nocode', 't_mcate', 'region', 'floor', 'area', 'date', 't
            'frc_cost', 'mth_profit', 'mth_rate', 'premium_term', 'deposit', 'mth_fee', 'total_fee']
 jumpo_df = pd.DataFrame(columns=columns)  # 빈 데이터 프레임
 
-for i in range(8): #range(len(type_df)):
+for i in range(len(type_df)):
 
     # 업종 코드 할당
     mcode = type_df['mcode'][i]
@@ -45,71 +45,89 @@ for i in range(8): #range(len(type_df)):
 
     # 웹페이지에서 element 가져오기
     for j in range(len(base_xpath)):
-        for k in range(jumpo_len[j]):
 
-            # element 경로
-            element_xpath = os.path.join(base_xpath[j], f'li[{k+1}]/div/div/div[2]/')
+        page = 1
 
-            # 기본 정보
-            nocode = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[1]/strong').text  # 매물번호
-            t_mcate = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[2]/strong').text  # 업종
-            region = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[2]').text[:-len(t_mcate)]  # 지역
-            floor = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[3]/strong').text  # 층
-            area = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[4]').text  # 면적
-            date = driver.find_element(By.XPATH, f'{element_xpath}div[1]/div/span[1]').text[11:]  # 실매물 주인확인 날짜
-            hits = driver.find_element(By.XPATH, f'{element_xpath}div[1]/div/span[2]').text[6:]  # 조회수
-            title = driver.find_element(By.XPATH, f'{element_xpath}h4').text  # 가게 이름
+        while True: # 페이지를 끝까지 반복
+            print(f"현재 페이지: {page}")
 
-            # 지도 링크
-            try:
-                map_link = driver.find_element(By.XPATH, f'{element_xpath}a').get_attribute('href')
-            except:
-                map_link = ''
+            for k in range(jumpo_len[j]):
 
-            # 가맹점 이름
-            try:
-                frc_name = driver.find_element(By.XPATH, f'{element_xpath}div[1]/a/span').text
-            except:
-                frc_name = ''
+                # element 경로
+                element_xpath = os.path.join(base_xpath[j], f'li[{k+1}]/div/div/div[2]/')
 
-            # 가격 정보
-            try:
-                test = driver.find_element(By.XPATH, f'{element_xpath}p[3]/span[1]/strong').text
-                p = 3
-            except:
-                p = 2
+                # 기본 정보
+                nocode = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[1]/strong').text  # 매물번호
+                t_mcate = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[2]/strong').text  # 업종
+                region = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[2]').text[:-len(t_mcate)]  # 지역
+                floor = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[3]/strong').text  # 층
+                area = driver.find_element(By.XPATH, f'{element_xpath}div[1]/span[4]').text  # 면적
+                date = driver.find_element(By.XPATH, f'{element_xpath}div[1]/div/span[1]').text[11:]  # 실매물 주인확인 날짜
+                hits = driver.find_element(By.XPATH, f'{element_xpath}div[1]/div/span[2]').text[6:]  # 조회수
+                title = driver.find_element(By.XPATH, f'{element_xpath}h4').text  # 가게 이름
 
-            premium = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[1]/strong').text  # 권리금
-            frc_cost = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[2]').text[5:]  # 가맹비용
-            mth_profit = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[3]').text[4:]  # 월수익
-            mth_rate = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[4]').text[5:]  # 월수익률
+                # 지도 링크
+                try:
+                    map_link = driver.find_element(By.XPATH, f'{element_xpath}a').get_attribute('href')
+                except:
+                    map_link = ''
 
-            standard = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]').text
-            if '권리금 회수기간' in standard:
-                premium_term = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]/strong').text  # 권리금 회수기간
-                deposit = ''  # 보증금
-                mth_fee = ''  # 월세
-                total_fee = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[6]/span/strong').text  # 총투자액
-            elif '보증금' in standard:
-                premium_term = ''  # 권리금 회수기간
-                deposit = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]/span[1]/strong').text  # 보증금
-                mth_fee = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]/span[2]/strong').text  # 월세
-                total_fee = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]/span[3]/strong').text  # 총투자액
+                # 가맹점 이름
+                try:
+                    frc_name = driver.find_element(By.XPATH, f'{element_xpath}div[1]/a/span').text
+                except:
+                    frc_name = ''
 
-            # 크롤링 source
+                # 가격 정보
+                try:
+                    test = driver.find_element(By.XPATH, f'{element_xpath}p[3]/span[1]/strong').text
+                    p = 3
+                except:
+                    p = 2
+
+                premium = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[1]/strong').text  # 권리금
+                frc_cost = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[2]').text[5:]  # 가맹비용
+                mth_profit = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[3]').text[4:]  # 월수익
+                mth_rate = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[4]').text[5:]  # 월수익률
+
+                standard = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]').text
+                if '권리금 회수기간' in standard:
+                    premium_term = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]/strong').text  # 권리금 회수기간
+                    deposit = ''  # 보증금
+                    mth_fee = ''  # 월세
+                    total_fee = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[6]/span/strong').text  # 총투자액
+                elif '보증금' in standard:
+                    premium_term = ''  # 권리금 회수기간
+                    deposit = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]/span[1]/strong').text  # 보증금
+                    mth_fee = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]/span[2]/strong').text  # 월세
+                    total_fee = driver.find_element(By.XPATH, f'{element_xpath}p[{p}]/span[5]/span[3]/strong').text  # 총투자액
+
+                # 크롤링 source
+                ad_type = ['업종별 프리미엄 광고', '업종별 일반 광고'][j]
+
+                # 데이터 프레임 병합
+                tmp_list = [ad_type, nocode, t_mcate, region, floor, area, date, title, map_link, frc_name, premium, frc_cost,
+                            mth_profit, mth_rate, premium_term, deposit, mth_fee, total_fee]
+                tmp_df = pd.DataFrame(data=[tmp_list], columns=columns)
+
+                jumpo_df = pd.concat([jumpo_df, tmp_df])
+
+            # 페이지 반복
             if j == 0:
-                ad_type = '업종별 프리미엄 광고'
-            elif j == 1:
-                ad_type = '업종별 일반 광고'
+                break
+            else:
+                try:
+                    next_page = driver.find_element(By.XPATH, f'//*[@id="dvPaging"]/div/div/a[{page}]')
+                    next_page.click()
+                    page += 1
+                    time.sleep(3)  # 페이지 로딩 대기
+                except:
+                    #print(f"{type_df['type1'][i]}-{type_df['type2'][i]} - 총 {page} 페이지 완료")
+                    print(f"일반음식점-돈까스/우동 - 총 {page} 페이지 완료")
+                    break  # 다음 버튼이 없으면 종료
 
-            # 데이터 프레임 병합
-            tmp_list = [ad_type, nocode, t_mcate, region, floor, area, date, title, map_link, frc_name, premium, frc_cost,
-                        mth_profit, mth_rate, premium_term, deposit, mth_fee, total_fee]
-            tmp_df = pd.DataFrame(data=[tmp_list], columns=columns)
-
-            jumpo_df = pd.concat([jumpo_df, tmp_df])
-
-    print(f"{type_df['type1'][i]}-{type_df['type2'][i]} 크롤링 완료")
+    #print(f"{type_df['type1'][i]}-{type_df['type2'][i]} 크롤링 완료")
+    print(f"일반음식점-돈까스/우동 크롤링 완료")
 
 # index 리셋
 jumpo_df.reset_index(drop=True, inplace=True)
